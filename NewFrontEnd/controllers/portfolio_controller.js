7263840e2fd8f7847c9d9fed1e7c1449db4a0c45
Luis N Cervantes <lcervant@hotmail.com>
2018-03-22 22:24:44 -0400

    new directories

--- NewFrontEnd/controllers/portfolio_controller.js
+++
@@ -1,8 +0,0 @@
-var db = require("../models");
-
-module.exports = {
-    projectSelect: function(callback){
-        db.Project.findAll() //call ORM on model
-        .then(callback); //then return data through callback
-    }
-}
